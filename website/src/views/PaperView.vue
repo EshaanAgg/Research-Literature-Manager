8610397576045e08fd3ad67375db04c83f7d66de
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2022-10-24 05:39:27 +0530

    Add url to open papers

--- website/src/views/PaperView.vue
+++ website/src/views/PaperView.vue
@@ -7,17 +7,19 @@
 </style>
 
 <template>
-  <h1>Current Papers</h1>
-  <br />
-  <ag-grid-vue
-    class="ag-theme-alpine papers-grid"
-    :columnDefs="columnDefs.value"
-    :rowData="rowData"
-    :defaultColDef="defaultColDef"
-    rowSelection="multiple"
-    animateRows="true"
-  >
-  </ag-grid-vue>
+  <div>
+    <h1>Current Papers</h1>
+    <br />
+    <ag-grid-vue
+      class="ag-theme-alpine papers-grid"
+      :columnDefs="columnDefs.value"
+      :rowData="rowData"
+      :defaultColDef="defaultColDef"
+      rowSelection="multiple"
+      animateRows="true"
+    >
+    </ag-grid-vue>
+  </div>
 </template>
 
 <script setup>
@@ -31,16 +33,34 @@ const rowData = ref([]);
 
 const columnDefs = ref({
   value: [
-    { headerName: "ID", field: "id", maxWidth: 80 },
-    { field: "title", minWidth: 180 },
-    { field: "venue", minWidth: 100 },
-    { field: "year", minWidth: 80 },
-    { headerName: "Citation Count", field: "citationCount" },
+    {
+      field: "title",
+      minWidth: 180,
+      width: 180,
+      cellRenderer: function (params) {
+        return `<a href="${params.data.url}" target="_blank">${params.data.title}</a>`;
+      },
+    },
+    { field: "venue", minWidth: 80, width: 80 },
+    { field: "year", maxWidth: 80 },
+    {
+      headerName: "Citation Count",
+      field: "citationCount",
+      width: 60,
+      minWidth: 60,
+    },
     {
       headerName: "Influential Citation Count",
       field: "influentialCitationCount",
+      width: 60,
+      minWidth: 60,
     },
-    { headerName: "Reference Count", field: "referenceCount" },
+    {
+      headerName: "Reference Count",
+      field: "referenceCount",
+      width: 60,
+      minWidth: 60,
+    },
     { field: "summary" },
     { field: "authors" },
   ],
